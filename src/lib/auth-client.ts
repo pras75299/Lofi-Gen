b69707ee01120a24b5bae972c50627567af92092
pras75299 <singh.prashantking@gmail.com>
2026-04-28 18:30:20 +0530

    Remove sign-in/sign-out auth flow and related components

--- src/lib/auth-client.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-    baseURL: "https://ep-cool-lake-afjqtwdf.neonauth.c-2.us-west-2.aws.neon.tech/neondb/auth"
-});
